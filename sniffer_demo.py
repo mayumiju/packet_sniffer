429a2da10085505cf89d60312a7ca6e80f48a49e
julia <julia@kali>
2024-04-30 19:59:39 -0300

    Unpacking ICMP and TCP packets

--- sniffer_demo.py
+++ sniffer_demo.py
@@ -34,4 +34,21 @@ def ipv4_packet(data):
 def ipv4(addr):
     return '.'.join(map(str, addr))
 
+# Unpacks ICMP packet
+def icmp_packet(data):
+    icmp_type, code, checksum = struct.unpack('! B B H', data[:4])
+    return icmp_packet, code, checksum, data[4:]
+
+# Unpacks TCP segment
+def tcp_segment(data):
+    (src_port, dest_port, sequence, acknowledgement, offset_reserved_flags) = struct.unpack('! H H L L H', data[:14])
+    offset = (offset_reserved_flags >> 12) * 4
+    flags_urg = (offset_reserved_flags & 32) >> 5
+    flags_ack = (offset_reserved_flags & 16) >> 4
+    flags_psh = (offset_reserved_flags & 8) >> 3
+    flags_rst = (offset_reserved_flags & 4) >> 2
+    flags_syn = (offset_reserved_flags & 2) >> 1
+    flags_fin = offset_reserved_flags & 1
+    return src_port, dest_port, sequence, acknowledgement, flags_ack, flags_fin, flags_psh, flags_rst, flags_syn, flags_urg, data[offset:]
+
 main ()
