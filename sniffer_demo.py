f06b74d1f875f34da0d4113fceb094bfc4e6ce91
julia <julia@kali>
2024-04-30 15:17:35 -0300

    basic structure p2

--- sniffer_demo.py
+++ sniffer_demo.py
@@ -6,3 +6,8 @@ import textwrap
 def ethernet_frame(data):
     dest_mac, src_mac, proto = struct.unpack ('! 6s, 6s, H', data[:14])
     return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto), data[14:S]
+
+# Return properly formatted MAC address (ie AA:BB:CC:DD:EE:FFS)
+def get_mac_addr(bytes_addr):
+    bytes_str = map('{:02x}'.format, bytes_addr)
+    return ':'.join(bytes_str).upper()
