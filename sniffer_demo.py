4050a5ac31ebdcb656b48356ed1af02089d4f5dc
julia <julia@kali>
2024-04-30 19:40:22 -0300

    Adding analysis of IP packet and getting the version, header length, ttl, src, target, proto and data parameters

--- sniffer_demo.py
+++ sniffer_demo.py
@@ -22,4 +22,16 @@ def get_mac_addr(bytes_addr):
     bytes_str = map('{:02x}'.format, bytes_addr)
     return ':'.join(bytes_str).upper()
 
+# Unpacks IPv4 packet
+def ipv4_packet(data):
+    version_header_length = data[0]
+    version = version_header_length >> 4
+    header_length = (version_header_length & 15) * 4
+    ttl, proto, src, target = struct.unpack('! 8x B B 2x 4s 4s', data[:20])
+    return version, header_length, ttl, proto, ipv4(src), ipv4(target), data[header_length:]
+
+# Returns properly formatted IPv4 address
+def ipv4(addr):
+    return '.'.join(map(str, addr))
+
 main ()
